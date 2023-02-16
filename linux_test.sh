74c84ed87ed9a8d9309306b58bcb10151a3eb3e7
kbys <work4kbys@gmail.com>
2023-02-17 01:34:01 +0900

    fix rpath linux test

--- linux_test.sh
+++ linux_test.sh
@@ -3,4 +3,5 @@
 cp -R assets linux
 clang -Wall -std=c11 -O3 test.c -o linux/test \
   -Lmacos/lib -Imacos/include \
-  -lSDL2 -lSDL2_image -lSDL2_mixer -lGL
+  -lSDL2 -lSDL2_image -lSDL2_mixer -lGL \
+  -Wl,-rpath=\$ORIGIN/lib
