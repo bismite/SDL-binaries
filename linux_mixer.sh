1497690fc5e246fdc72ee5960654066f4ada8b9d
kbys <work4kbys@gmail.com>
2023-02-16 14:00:13 +0900

    fix linux_mixer.sh

--- linux_mixer.sh
+++ linux_mixer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-export PATH=$(pwd)/tmp/bin:$PATH
+export PATH=$(pwd)/linux/bin:$PATH
 mkdir -p linux/sdl_mixer
 rm -rf linux/SDL2_mixer-2.6.3
 tar xf SDL2_mixer-2.6.3.tar.gz -C linux
 
-cmake -B linux/sdl_mixer linux/SDL2_mixer-2.6.3 -DCMAKE_PREFIX_PATH=tmp \
+cmake -B linux/sdl_mixer linux/SDL2_mixer-2.6.3 -DCMAKE_PREFIX_PATH=linux \
   -DSDL2MIXER_FLAC=OFF -DSDL2MIXER_MOD=OFF -DSDL2MIXER_MIDI=OFF \
   -DSDL2MIXER_OPUS=OFF -DSDL2MIXER_MP3_DRMP3=ON -DSDL2MIXER_VORBIS_STB=ON
 cmake --build linux/sdl_mixer --parallel
