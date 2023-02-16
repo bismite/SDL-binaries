4a0d59ecb8736186596d834d37d73a7e6a6ae416
kbys <work4kbys@gmail.com>
2023-02-16 13:57:34 +0900

    fix

--- linux_sdl.sh
+++ linux_sdl.sh
@@ -4,6 +4,6 @@ mkdir -p linux/sdl
 rm -rf linux/SDL2-2.26.3
 tar xf SDL2-2.26.3.tar.gz -C linux
 
-cmake -B linux/sdl build/SDL2-2.26.3/ -DCMAKE_PREFIX_PATH=linux
+cmake -B linux/sdl linux/SDL2-2.26.3/ -DCMAKE_PREFIX_PATH=linux
 cmake --build linux/sdl --parallel
 cmake --install linux/sdl --prefix linux
