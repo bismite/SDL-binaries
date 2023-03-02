5ce9f3c80dd432aeebd26c228ac7135db9c91fa6
kbys <work4kbys@gmail.com>
2023-03-02 12:24:28 +0900

    linux : use short soname

--- linux_pack.sh
+++ linux_pack.sh
@@ -2,6 +2,9 @@
 
 (
 cd linux ;
+patchelf --set-soname libSDL2.so       lib/libSDL2.so ;
+patchelf --set-soname libSDL2_image.so lib/libSDL2_image.so ;
+patchelf --set-soname libSDL2_mixer.so lib/libSDL2_mixer.so ;
 tar -c -h -f SDL.tgz include licenses \
-  lib/libSDL2-2.0.so.0 lib/libSDL2_image-2.0.so.0 lib/libSDL2_mixer-2.0.so.0
+  lib/libSDL2.so lib/libSDL2_image.so lib/libSDL2_mixer.so ;
 )
