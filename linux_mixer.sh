cf31fb5d048e381e951e925001aec851ed7e8f95
kbys <work4kbys@gmail.com>
2023-04-10 06:41:41 +0900

    add static libs in linux. update emscripten 3.1.35

--- linux_mixer.sh
+++ linux_mixer.sh
@@ -7,9 +7,17 @@ mkdir -p linux/sdl_mixer
 rm -rf /tmp/${SDL_MIXER}
 tar xf ${SDL_MIXER}.tar.gz -C /tmp
 
-cmake -B linux/sdl_mixer /tmp/${SDL_MIXER} -DCMAKE_PREFIX_PATH=linux \
-  -DSDL2MIXER_FLAC=OFF -DSDL2MIXER_MOD=OFF -DSDL2MIXER_MIDI=OFF \
-  -DSDL2MIXER_OPUS=OFF -DSDL2MIXER_MP3_DRMP3=ON -DSDL2MIXER_VORBIS_STB=ON
+OPTS=$(echo -DCMAKE_PREFIX_PATH=linux \
+ -DSDL2MIXER_FLAC=OFF -DSDL2MIXER_MOD=OFF -DSDL2MIXER_MIDI=OFF \
+ -DSDL2MIXER_OPUS=OFF -DSDL2MIXER_MP3_DRMP3=ON -DSDL2MIXER_VORBIS_STB=ON)
+
+echo ${OPTS}
+
+cmake -B linux/sdl_mixer-static /tmp/${SDL_MIXER} ${OPTS} -DBUILD_SHARED_LIBS=OFF
+cmake --build linux/sdl_mixer-static --parallel
+cmake --install linux/sdl_mixer-static --prefix linux
+
+cmake -B linux/sdl_mixer /tmp/${SDL_MIXER} ${OPTS} -DBUILD_SHARED_LIBS=ON
 cmake --build linux/sdl_mixer --parallel
 cmake --install linux/sdl_mixer --prefix linux
 
--- linux_pack.sh
+++ linux_pack.sh
@@ -6,5 +6,6 @@ patchelf --set-soname libSDL2.so       lib/libSDL2.so ;
 patchelf --set-soname libSDL2_image.so lib/libSDL2_image.so ;
 patchelf --set-soname libSDL2_mixer.so lib/libSDL2_mixer.so ;
 tar -c -h -f SDL.tgz include licenses \
-  lib/libSDL2.so lib/libSDL2_image.so lib/libSDL2_mixer.so ;
+  lib/libSDL2.so lib/libSDL2_image.so lib/libSDL2_mixer.so \
+  lib/libSDL2.a lib/libSDL2main.a lib/libSDL2_image.a lib/libSDL2_mixer.a ;
 )
